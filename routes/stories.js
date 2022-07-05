482e4b083ce7f673a68c9f59d68fb45fd968c2fa
Brian Mungai <bryanmungai254@gmail.com>
2022-07-05 11:11:01 +0300

    added get single user stories

--- routes/stories.js
+++ routes/stories.js
@@ -37,19 +37,56 @@ router.get("/", ensureAuth, async (req, res) => {
   }
 });
 
+//@desc show single story
+//@route GET /stories/:id
+router.get("/:id", ensureAuth, async (req, res) => {
+  try {
+    let story = await Story.findById(req.params.id).populate("user").lean();
+
+    if (!story) {
+      return res.render("errors/404");
+    }
+
+    res.render("stories/singleStory", { story });
+  } catch (error) {
+    console.log(error);
+    res.render("errors/500");
+  }
+});
+
+//@desc user's stories
+//@route GET /stories/user/:userId
+router.get("/user/:userId", ensureAuth, async (req, res) => {
+  try {
+    const stories = await Story.find({ user: req.params.userId })
+      .populate("user")
+      .lean();
+
+    res.render("stories/stories-list", { stories });
+  } catch (error) {
+    console.log(error);
+    res.render("errors/500");
+  }
+});
+
 //@desc show edit page
 //@route GET /stories/edit/:id
 router.get("/edit/:id", ensureAuth, async (req, res) => {
-  const story = await Story.findOne({ _id: req.params.id }).lean();
+  try {
+    const story = await Story.findOne({ _id: req.params.id }).lean();
 
-  if (!story) {
-    return res.render("error/404");
-  }
+    if (!story) {
+      return res.render("errors/404");
+    }
 
-  if (story.user != req.user.id) {
-    res.redirect("/stories");
-  } else {
-    res.render("stories/edit", { story });
+    if (story.user != req.user.id) {
+      res.redirect("/stories");
+    } else {
+      res.render("stories/edit", { story });
+    }
+  } catch (error) {
+    console.log(error);
+    res.render("errors/500");
   }
 });
 
@@ -71,12 +108,10 @@ router.put("/:id", ensureAuth, async (req, res) => {
         runValidators: true,
       });
       res.redirect("/dashboard");
-      console.log(error);
-      return res.render("errors/500");
     }
   } catch (error) {
     console.log(error);
-    return res.render("errors/500");
+    res.render("errors/500");
   }
 });
 
