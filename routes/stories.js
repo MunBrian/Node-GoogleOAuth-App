64418846c09b4f8f81cfd1e1b861ce82553d1d00
Brian Mungai <bryanmungai254@gmail.com>
2022-07-05 10:44:06 +0300

    added DELETE functionality in stories route

--- routes/stories.js
+++ routes/stories.js
@@ -56,22 +56,40 @@ router.get("/edit/:id", ensureAuth, async (req, res) => {
 //@desc update story
 //@route PUT /stories/:id
 router.put("/:id", ensureAuth, async (req, res) => {
-  const story = await Story.findById(req.params.id).lean();
+  try {
+    const story = await Story.findById(req.params.id).lean();
 
-  if (!story) {
-    return res.render("errors/404");
+    if (!story) {
+      return res.render("errors/404");
+    }
+
+    if (story.user != req.user.id) {
+      res.redirect("/stories");
+    } else {
+      await Story.findOneAndUpdate({ _id: req.params.id }, req.body, {
+        new: true,
+        runValidators: true,
+      });
+      res.redirect("/dashboard");
+      console.log(error);
+      return res.render("errors/500");
+    }
+  } catch (error) {
+    console.log(error);
+    return res.render("errors/500");
   }
+});
 
-  if (story.user != req.user.id) {
-    res.redirect("/stories");
-  } else {
-    await Story.findOneAndUpdate({ _id: req.params.id }, req.body, {
-      new: true,
-      runValidators: true,
-    });
+//@desc delete story
+//@route DELETE /stories/:id
+router.delete("/:id", ensureAuth, async (req, res) => {
+  try {
+    await Story.remove({ _id: req.params.id });
+    res.redirect("/dashboard");
+  } catch (error) {
+    console.log(error);
+    return res.render("errors/500");
   }
-
-  res.redirect("/dashboard");
 });
 
 module.exports = router;
